fc78d8092e3ecd665b805ff333c3e806385db948
szymoonio <zyszeksimon2005@gmail.com>
2026-02-23 12:57:12 +0100

    Finish task 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = "";
+    for (let i = 1; i <= string.length; i++) {
+        reverse += string.at(-i);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
