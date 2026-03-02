24db6e5a5ca8a4ed574a1e5ac6949c17648f209c
szymoonio <zyszeksimon2005@gmail.com>
2026-03-02 22:11:59 +0100

    Solve task 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (typeof(start) !== 'number' || typeof(end) !== 'number' || Math.floor(start) != start ||
+    Math.floor(end) != end || start < 0 || end < 0) {
+        return 'ERROR';
+    }
+    let sum = 0;
+    startPoint = start < end ? start : end;
+    endPoint = start < end ? end : start;
+    i = startPoint;
+    while (i <= endPoint) {
+        sum += i;
+        i++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
