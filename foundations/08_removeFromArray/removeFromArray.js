73c0789b56842387cf4eedc119027dc8cee83f79
szymoonio <zyszeksimon2005@gmail.com>
2026-03-02 19:18:07 +0100

    Solve task 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    for (const element of elements) {
+        let isIn = array.indexOf(element);
+        do {
+            console.log(isIn);
+            if (isIn != -1) {
+                array.splice(isIn, 1);
+            isIn = array.indexOf(element);
+            }
+        } while (isIn > 0);
+    }
+    return array;
 };
 
 // Do not edit below this line
