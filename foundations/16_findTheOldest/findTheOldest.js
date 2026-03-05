b3c87b5e49ae9a7e2ebb04454b00fd4a99501a27
szymoonio <zyszeksimon2005@gmail.com>
2026-03-05 15:09:54 +0100

    Solve task 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    for (let person of arr) {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = 2026;
+        }
+    }
+    return arr.sort((a, b) => ((b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)))[0];
 };
 
 // Do not edit below this line
